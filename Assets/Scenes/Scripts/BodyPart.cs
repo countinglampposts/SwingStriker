fdbc82c8d1db3cdadff76fc3aeddd359ce18ef97
Chris <chris@livelikevr.com>
2018-10-11 22:39:33 -0400

    Initial commit, basic character made

---
+++ Assets/Scenes/Scripts/BodyPart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Characters
+{
+
+    public class BodyPart : MonoBehaviour
+    {
+        [SerializeField]
+        public Vector3 pivot;
+        [SerializeField]
+        public BodyPart[] children;
+
+        public void OnDrawGizmos()
+        {
+            Vector3 pivotPosition = transform.TransformPoint(pivot);
+            float iconSize = .1f;
+            Gizmos.DrawLine(pivotPosition + Vector3.up * iconSize, pivotPosition - Vector3.up * iconSize);
+            Gizmos.DrawLine(pivotPosition + Vector3.back * iconSize, pivotPosition - Vector3.back * iconSize);
+            Gizmos.DrawLine(pivotPosition + Vector3.right * iconSize, pivotPosition - Vector3.right * iconSize);
+            
+            Gizmos.color = Color.green;
+            if (children != null)
+            {
+                foreach (BodyPart child in children)
+                {
+                    if (child != null) Gizmos.DrawLine(transform.position, child.transform.position);
+                }
+            }
+            Gizmos.color = Color.white;
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/BodyRigger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+
+namespace Characters
+{
+
+    public class BodyRigger : MonoBehaviour
+    {
+        [SerializeField]
+        private BodyPart body;
+
+        private void Start()
+        {
+            Rig(body);
+        }
+
+        private static void Rig(BodyPart body)
+        {
+            Action<BodyPart, BodyPart, Action<BodyPart, BodyPart>> applyRecursive = null;
+            applyRecursive = (BodyPart parent, BodyPart target, Action<BodyPart, BodyPart> action) =>
+            {
+                action(parent, target);
+                foreach (var child in target.children)
+                {
+                    applyRecursive(target, child, action);
+                }
+            };
+
+            Action<BodyPart, BodyPart> addChildComponents = (BodyPart parent, BodyPart child) =>
+            {
+                var gameObject = child.gameObject;
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+                rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+                //rigidbody.useGravity = false;
+                if (parent != null)
+                {
+                    var joint = gameObject.AddComponent<SpringJoint>();
+                    joint.anchor = child.pivot;
+                    joint.axis = Vector3.forward;
+                    joint.spring = 10000;
+                    joint.damper = .0001f;
+                    joint.enableCollision = true;
+
+                    /*JointLimits limits = joint.limits;
+                    limits.min = 0;
+                    limits.bounciness = 0;
+                    limits.bounceMinVelocity = 0;
+                    limits.max = 10;
+                    joint.limits = limits;
+                    joint.useLimits = false;*/
+                }
+            };
+
+            Action<BodyPart, BodyPart> rigChildComponents = (BodyPart parent, BodyPart child) =>
+            {
+                if (parent != null)
+                {
+                    var childJoint = child.GetComponent<Joint>();
+                    childJoint.connectedBody = parent.GetComponent<Rigidbody>();
+                }
+            };
+
+            applyRecursive(null, body, addChildComponents);
+            applyRecursive(null, body, rigChildComponents);
+        }
+    }
+}
