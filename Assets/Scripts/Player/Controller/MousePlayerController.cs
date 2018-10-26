55e7d6c5d1496ac53eac647bd53659bd3ecf968c
Chris <countinglampposts@gmail.com>
2018-10-26 17:01:18 -0400

    Fixed the janky mouse control toggling

--- Assets/Scripts/Player/Controller/MousePlayerController.cs
+++ Assets/Scripts/Player/Controller/MousePlayerController.cs
@@ -7,6 +7,7 @@ using UniRx.Diagnostics;
 using System;
 using System.Linq;
 using InControl;
+using System.Collections.Generic;
 
 namespace Swing.Player
 {
@@ -20,36 +21,49 @@ namespace Swing.Player
         private void Start()
         {
             bool mouseEnabled = true;
+            List<Vector3> buffer = new List<Vector3>();
             Observable.EveryUpdate()
-                      .TakeUntilDestroy(this) 
+                      .TakeUntilDestroy(this)
                       .Where(_ => state.localPlayerControl.Value)
-                      .Select(_ => Input.mousePosition)
-                      .Buffer(60)
+                      .Do(_ => {
+                          buffer.Add(Input.mousePosition);
+                          if (buffer.Count >= 30) buffer.RemoveAt(0);
+                      })
+                      .Select(_ => buffer)
+                      .Skip(1)
                       .Select(positions => positions.Skip(1).Select((next, index) => next - positions[index]).Average(delta => delta.magnitude))
                       .Select(avg => avg > 1)
                       .DistinctUntilChanged()
                       .Subscribe(enabled => mouseEnabled = enabled);
+
+            Action resetMouseDirection = () => state.aimDirection.Value = playerCamera.ScreenToWorldPoint(Input.mousePosition) - bodyRoot.rootBodyPart.transform.position;
 
             Observable.EveryUpdate()
                       .TakeUntilDestroy(this)
                       .Where(_ => state.localPlayerControl.Value)
                       .Where(_ => mouseEnabled)
                       .Where(_ => playerCamera.pixelRect.Contains(Input.mousePosition))
-                      .Subscribe(_ => state.aimDirection.Value = playerCamera.ScreenToWorldPoint(Input.mousePosition) - bodyRoot.rootBodyPart.transform.position);
+                      .Subscribe(_ => resetMouseDirection());
 
             Observable.EveryUpdate()
                       .TakeUntilDestroy(this)
                       .Where(_ => state.localPlayerControl.Value)
                       .Where(_ => Input.GetKeyDown(KeyCode.Mouse0))
                       .Where(_ => playerCamera.pixelRect.Contains(Input.mousePosition))
-                      .Subscribe(_ => signalBus.Fire<GrapplingFiredSignal>());
+                      .Subscribe(_ => {
+                          resetMouseDirection();
+                          signalBus.Fire<GrapplingFiredSignal>(); 
+                      });
 
             Observable.EveryUpdate()
                       .TakeUntilDestroy(this)
                       .Where(_ => state.localPlayerControl.Value)
                       .Where(_ => Input.GetKeyUp(KeyCode.Mouse0))
                       .Where(_ => playerCamera.pixelRect.Contains(Input.mousePosition))
-                      .Subscribe(_ => signalBus.Fire(new GrapplingReleasedSignal()));
+                      .Subscribe(_ => {
+                          resetMouseDirection();
+                          signalBus.Fire<GrapplingReleasedSignal>();
+                      });
         }
     }
 }
