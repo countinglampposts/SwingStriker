d4cf76401b147c0e8dc62ed15f9d5e4f5231e946
Chris <countinglampposts@gmail.com>
2018-10-27 01:26:09 -0400

    fixed how grappling sound emitter works

--- Assets/Scripts/Sound/Component/GrapplingSoundEmitter.cs
+++ Assets/Scripts/Sound/Component/GrapplingSoundEmitter.cs
@@ -4,23 +4,22 @@ using System.Linq;
 using Swing.Character;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Audio;
 using Zenject;
 
 namespace Swing.Sound
 {
-    [RequireComponent(typeof(AudioSource))]
     public class GrapplingSoundEmitter : MonoBehaviour
     {
         [Inject] private SoundAssets sounds;
         [Inject] private SignalBus signalBus;
+        [Inject] private AudioMixerGroup audioMixerGroup;
 
         private void Start()
         {
-            var audioSource = GetComponent<AudioSource>();
-
             signalBus.GetStream<GrapplingFiredSignal>()
                      .TakeUntilDestroy(this)
-                     .Subscribe(_ => audioSource.PlayOneShot(sounds.sounds.FirstOrDefault(sound => sound.id == "Grappling").clip));
+                     .Subscribe(_ => AudioUtils.PlayAudioAtPosition(transform.position, sounds.sounds.FirstOrDefault(sound => sound.id == "Grappling").clip, audioMixerGroup));
         }
     }
 }
